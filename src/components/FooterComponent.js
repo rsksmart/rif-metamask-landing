3a0ef2dd786e2668305c7e98f63e48cab85bbbea
cesariovlabs <cesar@iovlabs.org>
2021-08-11 12:41:53 -0300

    PR#6

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -4,8 +4,6 @@ import {
 } from 'react-bootstrap'
 import logo from '../img/powered_by.png'
 
-import { version } from '../../package.json'
-
 const FooterComponent = () => {
   return (
     <footer id="footer">
@@ -34,34 +32,16 @@ const FooterComponent = () => {
                   <div className="footer-links">
                     <ul>
                       <li>
-                        <a href="http://www.rifos.org/assets/whitepapers/rif-whitepaper-en.pdf" >Whitepaper</a>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/assets/whitepapers/rif-whitepaper-en.pdf" >Whitepaper</a>
                       </li>
                       <li>
-                        <a href="http://www.rifos.org/blog/" >Blog</a>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/blog/" >Blog</a>
                       </li>
                       <li>
-                        <a href="http://www.rifos.org/roadmap" >Roadmap</a>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/roadmap" >Roadmap</a>
                       </li>
                       <li>
-                        <a href="https://github.com/riflabs" >Github</a>
-                      </li>
-                    </ul>
-                  </div>
-                </Col>
-                <Col md="4">
-                  <div className="footer-links">
-                    <ul>
-                      <li>
-                        <a href="http://www.rifos.org/faqs" >FAQ´s</a>
-                      </li>
-                      <li>
-                        <a href="http://www.rifos.org/assets/guidelines/RIF_FullBrandGuidelines_En.pdf" >Brand Guidelines</a>
-                      </li>
-                      <li>
-                        <a href="https://www.rsk.co/" className="medium">RSK</a>
-                      </li>
-                      <li>
-                        <a href="https://www.iovlabs.org/" className="medium">About IOVLabs</a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/riflabs" >Github</a>
                       </li>
                     </ul>
                   </div>
@@ -70,19 +50,37 @@ const FooterComponent = () => {
                   <div className="footer-links">
                     <ul>
                       <li>
-                        <a href="https://www.iovlabs.org/contact.html" className="medium">Contact IOVLabs</a>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/faqs" >FAQ´s</a>
                       </li>
                       <li>
-                        <a href="http://www.rifos.org/terms-conditions" >Terms & Conditions</a>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/assets/guidelines/RIF_FullBrandGuidelines_En.pdf" >Brand Guidelines</a>
                       </li>
                       <li>
-                        <a href="http://www.rifos.org/privacy-policy" >Privacy Policy</a>
+                        <a target="_blank" rel="noreferrer" href="https://www.rsk.co/" className="medium">RSK</a>
+                      </li>
+                      <li>
+                        <a target="_blank" rel="noreferrer" href="https://www.iovlabs.org/" className="medium">About IOVLabs</a>
+                      </li>
+                    </ul>
+                  </div>
+                </Col>
+                <Col md="4">
+                  <div className="footer-links">
+                    <ul>
+                      <li>
+                        <a target="_blank" rel="noreferrer" href="https://www.iovlabs.org/contact.html" className="medium">Contact IOVLabs</a>
+                      </li>
+                      <li>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/terms-conditions" >Terms & Conditions</a>
+                      </li>
+                      <li>
+                        <a target="_blank" rel="noreferrer" href="http://www.rifos.org/privacy-policy" >Privacy Policy</a>
                       </li>
                     </ul>
                   </div>
                   <div className="social-links">
-                    <a href="https://twitter.com/rif_os" rel="noopener noreferrer" target="_blank" className="twitter"><i className="fab fa-twitter"></i></a>
-                    <a href="https://t.me/rif_os_community" rel="noopener noreferrer" target="_blank" className="telegram"><i className="fab fa-telegram"></i></a>
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/rif_os" className="twitter"><i className="fab fa-twitter"></i></a>
+                    <a target="_blank" rel="noreferrer" href="https://t.me/rif_os_community" className="telegram"><i className="fab fa-telegram"></i></a>
                   </div>
                 </Col>
               </Row>
