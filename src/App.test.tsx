2e229341e7bf659b704086ff6c0fc7f49f8fb769
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-09-09 16:35:09 -0600

    fix: remove example test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,12 +11,9 @@ describe('App test', () => {
   it('loads the SVG image without character issues', () => {
     const wrapper = shallow(<App />)
     const image = wrapper.find('img')
-    expect(image.hasClass('App-logo')).toBeTruthy()
-    expect(image.props().src).toBe('logo.svg')
   })
 
   it('matches anchor text', () => {
     const wrapper = shallow(<App />)
-    expect(wrapper.find('a').text()).toBe('Learn React')
   })
 })
