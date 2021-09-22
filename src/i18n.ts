220b4f706ccd7f9807e7da53a12c9c440345a7b3
cesariovlabs <cesar@iovlabs.org>
2021-09-22 13:37:58 -0300

    Revert en config

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,6 +10,37 @@ const resources = {
   en: {
     name: 'English',
     translation: {
+      About: 'About',
+      'How to list your tokens': 'How to list your tokens',
+      'How to customize with your tokens': 'How to customize with your tokens',
+      'Read more': 'Read more',
+      'Github repo': 'Github repo',
+      'Connect Metamask to RSK': 'Connect Metamask to RSK',
+      'Use this tool to connect your Metamask browser wallet to the RSK network. After this steps you will be able to send tokens and connect to dapps.': 'Use this tool to connect your Metamask browser wallet to the RSK network. After this steps you will be able to send tokens and connect to dapps.',
+      'Download and setup Metamask.': 'Download and setup Metamask.',
+      'Download Metamask': 'Download Metamask',
+      'Connect your Metamask wallet.': 'Connect your Metamask wallet.',
+      'Connect Wallet': 'Connect Wallet',
+      'Click here to add the Mainnet or Testnet, or change to that network if you already have them configured.': 'Click here to add the Mainnet or Testnet, or change to that network if you already have them configured.',
+      'Connect to RSK': 'Connect to RSK',
+      'Add the RSK tokens! Click below to add the': 'Add the RSK tokens! Click below to add the',
+      'RSK compatible tokens.': 'RSK compatible tokens.',
+      Add: 'Add',
+      Token: 'Token',
+      'RIF enables broad interoperability and faster time-to-deployment, bridging the gap between blockchain technologies and their mass-market adoption.': 'RIF enables broad interoperability and faster time-to-deployment, bridging the gap between blockchain technologies and their mass-market adoption.',
+      Whitepaper: 'Whitepaper',
+      Blog: 'Blog',
+      Roadmap: 'Roadmap',
+      Github: 'Github',
+      'FAQ´s': 'FAQ´s',
+      'Brand Guidelines': 'Brand Guidelines',
+      RSK: 'RSK',
+      'About IOVLabs': 'About IOVLabs',
+      'Contact IOVLabs': 'Contact IOVLabs',
+      'Terms & Conditions': 'Terms & Conditions',
+      'Privacy Policy': 'Privacy Policy',
+      Copyright: 'Copyright',
+      'RSK Labs. All rights reserved.': 'RSK Labs. All rights reserved.'
     }
   }
 }
